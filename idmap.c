41f1fef9a008fe9e75adc7ac1f23e5ea5f01a3d8
Perry Lorier <git@isomer.meta.net.nz>
2014-09-10 23:13:01 +0100

    Cleanup duplicated code.

--- idmap.c
+++ idmap.c
@@ -8,12 +8,10 @@
 #include <string.h>
 #include <unistd.h>
 
-/* TODO: This file has a lot of duplicated code, refactor mercilessly. */
-
 static struct argp_option id_options[] = {
-    {"mapuid", 1080, "newuid[-newuid]=olduid", 0,
+    {"mapuid", 1080, "newuid[:endnewuid]=olduid", 0,
         "Map user range, may be provided multiple times", 0},
-    {"mapgid", 1081, "newgid[-newgid]=oldgid", 0,
+    {"mapgid", 1081, "newgid[:endnewgid]=oldgid", 0,
         "Map group range, may be provided multiple times", 0},
     {NULL, 0, NULL, 0, NULL, 0},
 };
@@ -40,58 +38,43 @@ static struct idmap_t *allocate_id(struct idmap_t **id_head,
 }
 
 
-static error_t parse_id_options(int key, char *arg, struct argp_state *state)
+static void add_item(struct argp_state *state, char *arg,
+        struct idmap_t **head, struct idmap_t **tail, char *name)
 {
     struct idmap_t *tmp;
     char *dot, *dot2;
+    tmp = allocate_id(head, tail);
+    errno = 0;
+    tmp->idstart = strtol(arg, &dot, 10);
+    if (errno != 0)
+        argp_failure(state, 1, 0, "Expected start id in %s map", name);
+    if (*dot == '-') {
+        tmp->idend = strtol(dot+1, &dot2, 10);
+        if (errno != 0)
+            argp_failure(state, 1, 0, "Expected end id in %s map", name);
+        dot = dot2;
+    }
+    else
+        tmp->idend = tmp->idstart;
+    if (*dot != '=')
+        argp_failure(state, 1, 0, "Expected = in %s map", name);
+    tmp->oldid = strtol(dot+1, &dot2, 10);
+    if (errno != 0)
+        argp_failure(state, 1, 0, "Expected old id in %s map", name);
+    if (*dot2 != '\0')
+        argp_failure(state, 1, 0, "Extra cruft after old id in %s map (%s)",
+                name, dot2);
+}
+
+
+static error_t parse_id_options(int key, char *arg, struct argp_state *state)
+{
     switch(key) {
         case 1080:
-            tmp = allocate_id(&uidmap_head, &uidmap_tail);
-            errno = 0;
-            tmp->idstart = strtol(arg, &dot, 10);
-            if (errno != 0)
-                argp_failure(state, 1, 0, "Expected start id in %s map", "uid");
-            if (*dot == '-') {
-                tmp->idend = strtol(dot+1, &dot2, 10);
-                if (errno != 0)
-                    argp_failure(state, 1, 0, "Expected end id in %s map",
-                            "uid");
-                dot = dot2;
-            }
-            else
-                tmp->idend = tmp->idstart;
-            if (*dot != '=')
-                argp_failure(state, 1, 0, "Expected = in %s map", "uid");
-            tmp->oldid = strtol(dot+1, &dot2, 10);
-            if (errno != 0)
-                argp_failure(state, 1, 0, "Expected old id in %s map", "uid");
-            if (*dot2 != '\0')
-                argp_failure(state, 1, 0, "Extra cruft after old id in %s map (%s)",
-                        "uid", dot2);
+            add_item(state, arg, &uidmap_head, &uidmap_tail, "uid");
             break;
         case 1081:
-            tmp = allocate_id(&gidmap_head, &gidmap_tail);
-            errno = 0;
-            tmp->idstart = strtol(arg, &dot, 10);
-            if (errno != 0)
-                argp_failure(state, 1, 0, "Expected start id in %s map", "uid");
-            if (*dot == '-') {
-                tmp->idend = strtol(dot+1, &dot2, 10);
-                if (errno != 0)
-                    argp_failure(state, 1, 0, "Expected end id in %s map",
-                            "uid");
-                dot = dot2;
-            }
-            else
-                tmp->idend = tmp->idstart;
-            if (*dot != '=')
-                argp_failure(state, 1, 0, "Expected = in %s map", "uid");
-            tmp->oldid = strtol(dot+1, &dot2, 10);
-            if (errno != 0)
-                argp_failure(state, 1, 0, "Expected old id in %s map", "uid");
-            if (*dot2 != '\0')
-                argp_failure(state, 1, 0, "Extra cruft after old id in %s map (%s)",
-                        "uid", dot2);
+            add_item(state, arg, &gidmap_head, &gidmap_tail, "gid");
             break;
         default:
             return ARGP_ERR_UNKNOWN;
@@ -103,52 +86,39 @@ struct argp idmap_argp = {
     id_options, parse_id_options, "", "ID Remapping options", 0, 0, 0 };
 
 
-int do_idmap(int ppid)
+static void write_map(struct idmap_t *head, int ppid, const char *map)
 {
     char filename[1024];
+    char *buffer = NULL;
     struct idmap_t *idmap;
+    for (idmap = head;
+            idmap;
+            idmap = idmap->next) {
+        char *tmpbuf = NULL;
+        asprintf(&tmpbuf, "%s%d %d %d\n",
+                buffer ? buffer : "",
+                idmap->idstart,
+                idmap->oldid,
+                idmap->idend - idmap->idstart + 1);
+        if (buffer)
+            free(buffer);
+        buffer = tmpbuf;
+    }
+    sprintf(filename, "/proc/%d/%s_map", ppid, map);
+    int fd = open(filename, O_RDWR);
+    write(fd, buffer, strlen(buffer));
+    close(fd);
+    free(buffer);
+}
+
+int do_idmap(int ppid)
+{
     if (gidmap_head) {
-        char *buffer = NULL;
-        for (idmap = gidmap_head;
-                idmap;
-                idmap = idmap->next) {
-            char *tmpbuf = NULL;
-            asprintf(&tmpbuf, "%s%d %d %d\n",
-                    buffer ? buffer : "",
-                    idmap->idstart,
-                    idmap->oldid,
-                    idmap->idend - idmap->idstart + 1);
-            if (buffer)
-                free(buffer);
-            buffer = tmpbuf;
-        }
-        sprintf(filename, "/proc/%d/gid_map", ppid);
-        int fd = open(filename, O_RDWR);
-        write(fd, buffer, strlen(buffer));
-        close(fd);
-        free(buffer);
+        write_map(gidmap_head, ppid, "gid");
     }
 
     if (uidmap_head) {
-        char *buffer = NULL;
-        for (idmap = uidmap_head;
-                idmap;
-                idmap = idmap->next) {
-            char *tmpbuf = NULL;
-            asprintf(&tmpbuf, "%s%d %d %d\n",
-                    buffer ? buffer : "",
-                    idmap->idstart,
-                    idmap->oldid,
-                    idmap->idend - idmap->idstart + 1);
-            if (buffer)
-                free(buffer);
-            buffer = tmpbuf;
-        }
-        sprintf(filename, "/proc/%d/uid_map", ppid);
-        int fd = open(filename, O_RDWR);
-        write(fd, buffer, strlen(buffer));
-        close(fd);
-        free(buffer);
+        write_map(uidmap_head, ppid, "uid");
     }
     return 0;
 }
